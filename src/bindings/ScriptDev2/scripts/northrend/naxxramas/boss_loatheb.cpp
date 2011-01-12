d8c063a7d11b9278c0c4c976f3ae20379ec82874
Grandres <grundil@wp.pl>
2011-01-12 20:45:36 +0100

    SD2 rewriting. Naxxramas

--- src/bindings/ScriptDev2/scripts/northrend/naxxramas/boss_loatheb.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/naxxramas/boss_loatheb.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2011 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -85,7 +85,7 @@ struct MANGOS_DLL_DECL boss_loathebAI : public ScriptedAI
     void JustReachedHome()
     {
         if (m_pInstance)
-            m_pInstance->SetData(TYPE_LOATHEB, NOT_STARTED);
+            m_pInstance->SetData(TYPE_LOATHEB, FAIL);
     }
 
     void JustSummoned(Creature* pSummoned)
