72bb15dbd8bef4f3ee2be190c7653a17dcbe5f64
koksneo <zadaniematma@interia.pl>
2011-01-21 17:20:17 +0100

    Fix typo from previous commit

--- DB_updates/mangos/to_update/a372_Q_The_Earhen_Oath.sql
+++ DB_updates/mangos/to_update/a372_Q_The_Earhen_Oath.sql
@@ -22,7 +22,7 @@ INSERT INTO creature_ai_scripts VALUES
 (2998403, 29984, 6, 0, 100, 0, 0, 0, 0, 0, 33, 30296, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Iron Sentinel - kill credit upon death');
 
 -- add Thorim(30295) to the world
-DELETE FROM creature WHERE guid = '13279687';
+DELETE FROM creature WHERE id = 30295;
 INSERT INTO creature (`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`) 
 VALUES (30295,571,1,1,0,0,7761.64,-2158.88,1233.33,1.50647,300,5,0,1,0,0,0);
 
