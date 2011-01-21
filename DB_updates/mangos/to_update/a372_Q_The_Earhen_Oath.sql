89d79020d648026bc3138e3f596ce64c6bab9a17
koksneo <zadaniematma@interia.pl>
2011-01-21 16:47:32 +0100

    Forgot to add Thorim into the world

--- DB_updates/mangos/to_update/a372_Q_The_Earhen_Oath.sql
+++ DB_updates/mangos/to_update/a372_Q_The_Earhen_Oath.sql
@@ -21,6 +21,11 @@ DELETE from creature_ai_scripts where id=2998403;
 INSERT INTO creature_ai_scripts VALUES
 (2998403, 29984, 6, 0, 100, 0, 0, 0, 0, 0, 33, 30296, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Iron Sentinel - kill credit upon death');
 
+-- add Thorim(30295) to the world
+DELETE FROM creature WHERE guid = '13279687';
+INSERT INTO creature (`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`DeathState`,`MovementType`) 
+VALUES (30295,571,1,1,0,0,7761.64,-2158.88,1233.33,1.50647,300,5,0,1,0,0,0);
+
 -- Eisenfaust
 
 UPDATE creature_template SET
