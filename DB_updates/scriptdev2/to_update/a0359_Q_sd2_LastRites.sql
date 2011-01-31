17695f231ff36ed17faec32aeec31dcd0d59c214
PanteraPolnocy <NorthPanther@interia.pl>
2011-01-31 04:39:12 +0100

    Better custom SQL's sorting, not a real commit... part 2 =='

---
+++ DB_updates/scriptdev2/to_update/a0359_Q_sd2_LastRites.sql
@@ -0,0 +1,23 @@
+DELETE FROM script_texts WHERE comment = 28498;
+insert into script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1534071, 'My liege, the infiltratio and control of the Alliance power sctructure by our cultists is well underway.', 14211,0,0,0, '28498'),
+(-1534072, 'The power you\'ve bestowed upon me has granted me great mental influence over human minds. I bear these offerings as proof of my progress.', 14212,0,0,0, '28498'),
+(-1534073, 'Your achievements in this region rather impress Prince of Blood. I am pleased!', 14756,0,0,1, '28498'),
+(-1534074, 'Leryssa!', 14664,1,0,0, '28498'),
+(-1534084, 'Thassarian! You\'re alive!',14489,0,0,0, '28498'),
+(-1534075, 'What have you done to my sister, you motherless elf scum!?', 14665,1,0,0, '28498'),
+(-1534076, 'Now this is surprise, Thassarian. I hadn\'t heard from Mograine of the other deathknights for months. You\'ve come to rejoin the Scourge. I take it?', 14757,0,0,1, '28498'),
+(-1534077, 'I would sooner slit my own throat. You will pay for what you did to your own men. Arthas... for what you did do me! I swear it.', 14666,1,0,0, '28498'),
+(-1534078, 'Allow me to take care of the intruders, lord. I will feed their entrails to the maggots.', 14213,0,0,1, '28498'),
+(-1534079, 'Do not fail me, San\'layn. Return to Icecrown with this fool\'s head or do not bother to return.',14758,0,0,1, '28498'),
+(-1534080, 'Yes, my lord!', 14214,0,0,0, '28498'),
+(-1534083, 'Leryssa... you... you\'re all right!', 14667,0,0,0, '28498'),
+(-1534085, 'I thought... I thought you were... dead.', 14490,0,0,0, '28498'),
+(-1534087, 'Don\'t leave me again! You want to fight for your country, but they don\'t even want you! They sent you here to die!', 14491,0,0,0, '28498'),
+(-1534086, 'I cannot return with you just yet, Lerissa. I am not quite done with the Scourge.', 14668,0,0,0, '28498'),
+(-1534088, 'You might be right, sister. My obligations to my land and King have been fulfilled. But there is something that I still owe to myself.', 14669,0,0,0, '28498'),
+(-1534089, 'I know that look in your eye... I\'m not going to be able to talk you out of this. If you die on me again...', 14492,0,0,0, '28498'),
+(-1534090, 'Do not worry, Leryssa. I will come back yo you when I an done. Nothing in the world will stop me from coming home to the only family that I have left.', 14670,0,0,0, '28498'),
+(-1534081, 'What... what happened to me?', 14196,0,0,0, '28498'),
+(-1534082, 'I think... i think i\'m going to faint...', 14197,0,0,0, '28498');
+
