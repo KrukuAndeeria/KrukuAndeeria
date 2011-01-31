819e4a781fba42e1e1f834c54fbc787570336597
koksneo <zadaniematma@interia.pl>
2011-01-31 19:30:42 +0100

    ...

--- DB_updates/mangos/to_update/a0421_Q_Rejek_First_Blood.sql
+++ DB_updates/mangos/to_update/a0421_Q_Rejek_First_Blood.sql
@@ -4,7 +4,7 @@
 
 DELETE FROM `creature_ai_scripts` WHERE id = 2808602;
 INSERT INTO creature_ai_scripts 
-(`id`,`creature_id`,`event_type`,`event_inverse_ph, ase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`action2_type`,`action2_param1`,`action2_param2`,`action2_param3`,`action3_type`,`action3_param1`,`action3_param2`,`action3_param3`,`comment`)
+(`id`,`creature_id`,`event_type`,`event_inverse_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`action2_type`,`action2_param1`,`action2_param2`,`action2_param3`,`action3_type`,`action3_param1`,`action3_param2`,`action3_param3`,`comment`)
 VALUES (2808602,28086,8,0,100,0,52992,-1,0,0,33,28040,6,0,0,0,0,0,0,0,0,0,'Kill Credit - Rejeks Blade blooded on Sapphire Hive Wasp');
 
 DELETE FROM `creature_ai_scripts` WHERE id = 2809602;
